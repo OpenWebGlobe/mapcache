06c49ad8e4685acdae8fa8e23e6a496e6a720942
Martin Christen <martin.christen@fhnw.ch>
2013-01-14 14:51:30 +0100

    added code: heightmap to json

--- lib/imageio_json.c
+++ lib/imageio_json.c
@@ -29,29 +29,322 @@
 
 #include "mapcache.h"
 #include <apr_strings.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
 
+#ifndef M_PI
+#    define M_PI 3.14159265358979323846
+#endif
+//------------------------------------------------------------------------------
+// very simple high performance string handling for JSON strings.
+// dynamic strings based on: http://locklessinc.com/articles/dynamic_cstrings/
+  
+#define STR_FREEABLE (1ULL << 63)
+#define NaS ((json_string) {NULL, 0, 0, 0})
+#define STRINIT ((json_string) {(char*)malloc(16), 0, (16), (char*)malloc(32)})
+
+  
+typedef struct json_string json_string;
+struct json_string
+{
+  char *s;
+  size_t size;   // length of string
+  size_t b_size; // memory available
+  char *tmp;     // 32 bytes temp memory to speed up certain operations
+};
+//------------------------------------------------------------------------------- 
+static size_t json_strsize(json_string *s)
+{
+  if (!s->s) return 0;
+  return s->size;
+}
+//------------------------------------------------------------------------------- 
+static json_string json_strmalloc(size_t size)
+{
+  if (size < 16) size = 16;
+  return (json_string) {(char*)malloc(size), 0, size | STR_FREEABLE, (char*)malloc(32)};
+}
+//------------------------------------------------------------------------------- 
+// resize string-buffer
+static void _json_str_resize(json_string *s, size_t size)
+{
+  char *buf;
+  size_t bsize;
+  
+  if (!(s->s)) return;
+ 
+  if (!(s->b_size & STR_FREEABLE))
+  {
+    json_string s2;
+    if (size <= s->size) return;
+    s2 = json_strmalloc(size);
+    memcpy(s2.s, s->s, s->size);
+    s->s = s2.s;
+    s->b_size = s2.b_size;
+    
+    return;
+  }
+  
+  if (size & STR_FREEABLE)
+  {
+    free(s->s);
+    *s = NaS;
+    return;
+  }
+  
+  bsize = s->b_size - STR_FREEABLE;
+  if (size < 16) size = 16;
+  if ((4 * size > 3 * bsize) && (size <= bsize)) return;
+  if ((size > bsize) && (size < bsize * 2)) size = bsize * 2;
+  if (size < 16) size = 16;
+  buf = (char*)realloc(s->s, size);
+  
+  if (!buf)
+  {
+    free(s->s);
+    *s = NaS;
+  }
+  else
+  {
+    s->s = buf;
+    s->b_size = size | STR_FREEABLE;
+  }
+}
+//------------------------------------------------------------------------------
+// convert to c-string
+static char *json_tocstr(json_string *s)
+{
+  size_t bsize;
+  if (!s->s) return NULL;
+  
+  bsize = s->b_size & ~STR_FREEABLE;
+  
+  /* null terminate string: there must be space for appending 0 */
+  if (s->size == bsize)
+  {
+    _json_str_resize(s, bsize + 1);
+    if (!s->s) return NULL;
+  }
+  s->s[s->size] = 0;
+  return s->s;
+}
+//------------------------------------------------------------------------------
+static void json_strprintf(json_string *s, const char *fmt, ...)
+{
+  va_list v;
+  size_t len;
+  if (!(s->s)) *s = STRINIT;
+  if (!fmt) return;
+  va_start(v, fmt);
+  len = vsnprintf(NULL, 0, fmt, v) + 1;
+  va_end(v);
+  _json_str_resize(s, len);
+  if (!(s->s)) return;
+  va_start(v, fmt);
+  vsnprintf(s->s, len, fmt, v);
+  va_end(v);
+  s->size = len - 1;
+}
+//------------------------------------------------------------------------------
+static void json_append_cstr(json_string *s, const char *str)
+{
+  size_t len;
+  size_t bsize;
+  len = strlen(str);
+  if (!(s->s)) *s = STRINIT;
+  if (!str || !len) return;
+  bsize = s->b_size & ~STR_FREEABLE;
+  if (s->size + len >= bsize)
+  {
+    _json_str_resize(s, s->size + len);
+    if (!(s->s)) return;
+  }
+  memcpy(&s->s[s->size], str, len);
+  s->size += len;
+}
+//----------------------------------------------------------------------------
+static void json_append_float(json_string *s, float value)
+{
+  snprintf(s->tmp, 32, "%.7f", value);
+  json_append_cstr(s, s->tmp);
+}
+//----------------------------------------------------------------------------
+static void json_append_comma_float(json_string *s, float value)
+{
+  snprintf(s->tmp, 32, ",%.7f", value);
+  json_append_cstr(s, s->tmp);
+}
+//----------------------------------------------------------------------------
+static void json_append_double(json_string *s, double value)
+{
+  snprintf(s->tmp, 32, "%.15lf", value);
+  json_append_cstr(s, s->tmp);
+}
+//----------------------------------------------------------------------------
+static void json_append_comma_double(json_string *s, double value)
+{
+  snprintf(s->tmp, 32, ",%.15lf", value);
+  json_append_cstr(s, s->tmp);
+}
+//------------------------------------------------------------------------------
+static void json_append_json(json_string *s, const json_string *s2)
+{
+  json_append_cstr(s, s2->s);
+}
+//------------------------------------------------------------------------------
+static void json_strfree(json_string *s)
+{
+  if (s->b_size & STR_FREEABLE) free(s->s);
+  if (s->tmp) free(s->tmp);
+  *s = NaS;
+}
+//------------------------------------------------------------------------------
+inline void _MercatorToWGS84(double x, double y, double* lng, double* lat)
+{
+  *lat = M_PI/2.0 - 2.0 * atan(exp(-y*M_PI));
+  *lng = M_PI*x / 1.0;
+}
+//------------------------------------------------------------------------------
+inline void _WGS84ToCartesian(double lng, double lat, double elv, double* x, double * y, double *z)
+{
+  double sinlat = sin(lat);
+  double coslat = cos(lat);
+  double sinlong = sin(lng);
+  double coslong = cos(lng);
+  double Rn = 6378137.0 / sqrt(1.0-0.006694379990197*sinlat*sinlat);
+  *x = (Rn + elv) * coslat * coslong * 1.1920930376163765926810017443897e-7;
+  *y = (Rn + elv) * coslat * sinlong * 1.1920930376163765926810017443897e-7;
+  *z = (0.993305620011365*Rn + elv) * sinlat * 1.1920930376163765926810017443897e-7;
+}
+//------------------------------------------------------------------------------
+void _gen_json(json_string* str, float* heightmap, int gridsize, double x0, double y0, double x1, double y1)
+{
+  
+  double offsetx, offsety, offsetz;
+  
+  //-----------
+  // START JSON
+  //-----------
+  json_append_cstr(str, "{\n");
+  
+  //------------------
+  // Generate VERSION
+  //------------------
+  
+  json_append_cstr(str, "  \"Version\": \"1.1\",\n");
+
+  //-------------------------
+  // Generate VERTEX SEMANTIC
+  //-------------------------
+  
+  json_append_cstr(str, "  \"VertexSemantic\": \"pt\",\n");
+
+  //------------------
+  // Generate VERTICES
+  //------------------
+  
+  json_append_cstr(str, "  \"Vertices\": [");
+  
+  // x0, y0, x1, y1: Bounding Box in Mercator Coordinates
+  // lng0, lat0, lng1, lat1: Bounding box in WGS84 Coordinates (radiant)
+  double lng0, lat0, lng1, lat1;
+  double x_cart, y_cart, z_cart;
+  double lng, lat;
+  
+  double dH = (y1-y0)/(gridsize-1); // for x positions
+  double dW = (x1-x0)/(gridsize-1); // for y positions
+  float fdX = 1.0 / (gridsize-1);   // for texture coordinates (u,v)
+  int x, y;
+   
+  for (y=0;y<gridsize;y++)
+  {
+    for (x=0;x<gridsize;x++)
+    {
+      double x_coord = x0 + x*dW;
+      double y_coord = y0 + y*dH;
+     
+      double elevation = 0; // #todo: read from heightmap
+         
+      _MercatorToWGS84(x_coord,y_coord,&lng,&lat);
+      _WGS84ToCartesian(lng, lat, elevation, &x_cart, &y_cart, &z_cart);
+         
+      if (x==0 && y==0)
+      {
+        offsetx = x_cart;
+        offsety = y_cart;
+        offsetz = z_cart;
+            
+        // POSITION:
+        json_append_float(str,(float)(x_cart - offsetx));
+        json_append_comma_float(str,(float)(y_cart - offsety));
+        json_append_comma_float(str,(float)(z_cart - offsetz));
+      }
+      else
+      {
+        // POSITION
+        json_append_comma_float(str,(float)(x_cart - offsetx));
+        json_append_comma_float(str,(float)(y_cart - offsety));
+        json_append_comma_float(str,(float)(z_cart - offsetz));
+      }    
+      
+      // TEXCOORD:
+      json_append_comma_float(str,(float)(x*fdX));
+      json_append_comma_float(str,(float)(1.0f-y*fdX));
+      
+    }
+  }
+  
+  json_append_cstr(str, "],\n"); // end Vertices
+  
+  //------------------------
+  // Generate INDEX SEMANTIC
+  //------------------------
+  
+  json_append_cstr(str, "  \"IndexSemantic\": \"TRIANGLES\",\n");
+  
+  //----------------
+  // Generate OFFSET
+  //----------------
+  json_append_cstr(str, "  \"Offset\": [");
+  json_append_double(str, offsetx);
+  json_append_comma_double(str, offsety);
+  json_append_comma_double(str, offsetz);
+  json_append_cstr(str, "],\n"); // end Offset
+  
+  //-------------------
+  // Generate HEIGHTMAP
+  //-------------------
+  
+  json_append_cstr(str, "  \"HeightMap\": [");
+  
+  int i;
+  for (i=0;i<gridsize*gridsize;i++)
+  {
+    if (i==0)
+    {
+      json_append_float(str, heightmap[0]);
+    }
+    else 
+    {
+      json_append_comma_float(str, heightmap[i]);
+    }
+    
+  }
+  
+  json_append_cstr(str, "],\n"); // end HeightMap
+ 
+  //---------------
+  // TERMINATE JSON
+  //---------------
+  
+  json_append_cstr(str, "}\n");
+}
 
 //------------------------------------------------------------------------------
-// template for elevation json:
-static char* json  =
-  "{"
-  "   \"Version\": \"1.1\",\n"
-  "   \"Triangulation\": \"grid\",\n"
-  "   \"GridSize\": %i,\n"
-  "   \"VertexSemantic\": \"pt\",\n"
-  "   \"IndexSemantic\": \"TRIANGLES\",\n"
-  "   \"Vertices\": [%s],\n"
-  "   \"Offset\": [%.15lf, %.15lf, %.15lf],\n"
-  "   \"BoundingBox\": [[%.15lf, %.15lf, %.15lf],[%.15lf,%.15lf,%.15lf]],\n"
-  "   \"HeightMap\": [%s]\n"
-  "}";
-// Integer: Gridsize  (2,3,5,9,17)
-// String:  Comma separated vertex list
-// Floats: Offset x, Offset y, Offset z
-// Floats: bbxmin, bbymin, bbzmin, bbxmax, bbymax, bbzmax
-// String: Comma separated heightmap
-
-
 //------------------------------------------------------------------------------
 static mapcache_buffer* _mapcache_imageio_json_create_empty(mapcache_context *ctx, mapcache_image_format *format,
     size_t width, size_t height, unsigned int color)
